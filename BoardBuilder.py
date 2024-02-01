90819f3ff513a2c5b72d982fe95f58eef2c801bc
patripolli <143572467+patripolli@users.noreply.github.com>
2024-02-01 01:51:56 -0300

    Added Matrix to BoardBuilder
    After selecting the size, the function now creates a matrix and randomly assigns "wavy" characters to the matrix.

--- BoardBuilder.py
+++ BoardBuilder.py
@@ -1,19 +1,26 @@
 #This is the function to build the board.
 
 
-def new_board():
-    board_size = 0
-    while board_size == 0:
-      size = str(input("Please enter S / M / L for board size")).upper()
-      if size == "S":
-          board_size = 6
-      if size == "M":
-          board_size = 9
-      if size == "M":
-          board_size = 12
-      elif board_size == 0:
-          print("Invalid input. Please type only S / M / L")
+class Board:
+  def new_board():
+      board_size = 0
+      while board_size == 0:
 
-    return board_size
+        if size == "S":
+            board_size = 8
+        if size == "M":
+            board_size = 10
+        if size == "L":
+            board_size = 12
+        elif board_size == 0:
+            print("Invalid input. Please type only S / M / L")
+        board = np.zeros((board_size, board_size), str)
+        for i in range(board_size):
+          for h in range(board_size):
+            board[i][h] = choices(wavez)[0]
+      return board
+
+  def place_word(word, firstsquare, orientation):
+
 
 new_board()
