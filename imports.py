087096c56ef582a30e91b066475a91175ace5488
patripolli <143572467+patripolli@users.noreply.github.com>
2024-05-10 12:53:15 -0300

    Create imports.py
    File containing all necessary imports

---
+++ imports.py
@@ -0,0 +1,16 @@
+##----------------IMPORTS----------------
+!pip install PyDictionary
+!pip install seaborn
+import random as random
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn.objects as so
+import re
+from random import choices
+from numpy import matrix
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+np.set_printoptions(threshold=np.inf)
