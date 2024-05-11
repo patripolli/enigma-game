7b935a9ba0b9937b2a776ac38858a4235c9ee3a4
patripolli <143572467+patripolli@users.noreply.github.com>
2024-05-11 13:45:25 -0300

    Create Enigma.py
    Started the main game file with some explanations

---
+++ Enigma.py
@@ -0,0 +1,13 @@
+import imports
+print('-------------------------------ENIGMA-------------------------------\n\nWelcome to ENIGMA, a single-player game (at the moment) about decrypting communications during a war.\nENIGMA is entirely text-based, so lend me your eyes, mind and imagination as we play.')
+
+#print('Would you like to play the TUTORIAL? (Extremely recommended if you've never played the game')
+print('This is where I'd put my tutorial... IF I HAD ONE!! (Coming soon, I promise!)
+      \n\nFor now, this is the basic premise of the game:
+      \nEach player has a square board with ROWS (letters A-L) and COLUMNS (numbers 1-12), representing their area in the ocean.
+      \nEach player will place their ships in that area, represented by coded words, and attempt to decrypt the other player's code words to identify their ships.
+      
+      \n\nThe game is divided in two stages: PLACEMENT and DECRYPTION.
+      \n\nPLACEMENT
+      \nDuring the Placement stage, you will choose code-words to add to your board. Think of them as the ships in Battleships.
+      \nAccording to
