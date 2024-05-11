b55cd4b8ddcaa44d6f7d4b023b51650dec84c029
patripolli <143572467+patripolli@users.noreply.github.com>
2024-05-11 13:12:54 -0300

    Update GameActions.py
    Updated some functions and comments, added entire turn function

--- GameActions.py
+++ GameActions.py
@@ -1,56 +1,67 @@
-def update():
-  !pip install PyDictionary
-  !pip install seaborn
-  import random as random
-  import numpy as np
-  import pandas as pd
-  import matplotlib as mpl
-  import matplotlib.pyplot as plt
-  import seaborn as sns
-  import seaborn.objects as so
-  import re
-  from random import choices
-  from numpy import matrix
-  from PyDictionary import PyDictionary
-  dictionary=PyDictionary()
-  np.set_printoptions(threshold=np.inf)
+##---------------WORD LIST---------------
+#Function to make the basic word list
+def make_word_list():
+    full_word_list = []
+    with open(beginnerwords_file, "r") as beginner_words:
+      for a in beginner_words:
+        b = a.replace('\n', '')
+        full_word_list.append(b)
+    return full_word_list
 
-##-----------WORD PLACING FUNCTIONS---------------
+##---------------WORD PLACING FUNCTIONS---------------
 #Function to check if the word is in the English dictionary and whether it fits the board
-def word_check_player(board, player):
+def word_check_player(board, player, required):
     not_repeated = False
     while not_repeated == False:
-      word = input(f"Enter the word you’d like to add to the board:\n").title()
-      while len(word) < 2:
-        print ("One letter words are not allowed. Please enter a word with 2 or more letters.")
-        word = input(f"Enter the word you’d like to add to the board:\n").title()
-      while len(word) > len(board):
-        print (f"Your word is bigger than the board. Please enter a word with up to {len(board)} letters.")
-        word = input(f"Enter the word you’d like to add to the board:\n").title()
+      word = input(f"Enter the word you’d like to add to the board:      (Stuck? Type 'helpme' for a random word)\n").title()
+      if word == 'Helpme':
+          word = random.choice(word_matcher('.'*required))
+          print(f'Suggested word: {word.title()}')
+      if len(word) != required:
+        print (f"Please enter a {required}-letter word.")
+        word = input(f"Enter the word you’d like to add to the board:       (Stuck? Type 'helpme' for a random word)\n").title()
+        if word == 'Helpme':
+          word = random.choice(word_matcher('.'*required))
+          print(f'Suggested word: {word.title()}')
+      ####This USED to be the length check until I decided to add fixed word lengths. Leaving it here just in case.
+      #while len(word) < 2:
+        #print ("One letter words are not allowed. Please enter a word with 2 or more letters.")
+        #word = input(f"Enter the word you’d like to add to the board:\n").title()
+      #while len(word) > len(board):
+        #print (f"Your word is bigger than the board. Please enter a word with up to {len(board)} letters.")
+        #word = input(f"Enter the word you’d like to add to the board:\n").title()
       wordmatch = 0
       while wordmatch == 0:
         if dictionary.meaning(word,True) is not None:
             wordmatch += 1
             valword = word
+            if word not in beginner_word_list:
+              beginner_word_list.append(word)
+              new_words.append(word)
         else:
           print ("Word not found. Please make sure the word exists in the English language dictionary.")
           word = input(f"Enter the word you’d like to add to the board:\n").title()
+          if word == 'Helpme':
+            word = random.choice(word_matcher('.'*required))
+            print(f'Suggested word: {word.title()}')
       if valword in player.word_list:
         wordmatch = 0
         print ("Word already placed. Please choose a different word.")
       else:
         not_repeated = True
     return valword
+  
 
 #Same but for NPC
-def word_check_npc(board, npc):
+def word_check_npc(board, npc, required):
     word = ''
     not_repeated = False
     while not_repeated == False:
-      while len(word) < 2 or len(word) > len(board):
-          word = random.choice(beginner_word_list)
-          if word in npc.word_list:
-            word = random.choice(beginner_word_list)
+      #while len(word) < 2 or len(word) > len(board):
+          #word = random.choice(beginner_word_list)
+      word = random.choice(word_matcher('.'*required))
+      if word in npc.word_list:
+        word = random.choice(word_matcher('.'*required))
       wordmatch = 0
       while wordmatch == 0:
         if dictionary.meaning(word,True) is not None:
@@ -64,6 +75,7 @@ def word_check_npc(board, npc):
         not_repeated = True
     return valword
 
+#Define a random coordinate for horizontal word placement
 def random_start_coord():
     colcoord_all = random.randint(1, (board_len))
     rowcoord_first3 = random.randint(1, 3)
@@ -154,8 +166,8 @@ def coordinates(word, board, player):
           for letter in word.upper():
             if board[coordinate][horizontcoord] in wavez:
               empty += 1
-            if letter == board[coordinate][horizontcoord]:
-              nice_overlap += 1
+           #if letter == board[coordinate][horizontcoord]:
+              #nice_overlap += 1
             coordinate += 1
 
         #Check for horizontal overlap
@@ -164,24 +176,27 @@ def coordinates(word, board, player):
           for letter in word.upper():
             if board[vertcoord][coordinate] in wavez:
               empty += 1
-            if letter == board[vertcoord][coordinate]:
-              nice_overlap += 1
+            #if letter == board[vertcoord][coordinate]:
+              #nice_overlap += 1
             coordinate += 1
 
 
         #Allow word if no overlap or single overlap
         if empty == fullword:
           emptycheck = True
-        elif empty == fullword-1 and nice_overlap == 1:
-          emptycheck = True
+        #elif empty == fullword-nice_overlap:
+          #emptycheck = True
         else:
           lencheck = False
-          sizecheck = 0
           emptycheck = False
+          sizecheck = 0
+          nice_overlap = 0
+          empty = 0
           if player is p1:
             print("Those coordinates are occupied.")
 
       return vertcoord, horizontcoord, orient
+
 
 #Place the word in the board
 def actualy_place(word, board, vc, hc, orientation):
@@ -200,16 +215,20 @@ def actualy_place(word, board, vc, hc, orientation):
         hc += 1
     return board
 
-def place_word_player(board, player):
-    checkedword = word_check_player(board, player)
+
+#Entire word placement function for players
+def place_word_player(board, player, required):
+    checkedword = word_check_player(board, player, required)
     vertcoord, horizontcoord, orient = coordinates(checkedword, board, player)
     actualy_place(checkedword, board, vertcoord, horizontcoord, orient)
     player.word_list.append(checkedword)
     beginner_word_list.append(checkedword)
     return board
 
-def place_word_npc(board, player):
-    checkedword = word_check_npc(board, player)
+
+#Entire word placement function for NPCs
+def place_word_npc(board, player, required):
+    checkedword = word_check_npc(board, player, required)
     vertcoord, horizontcoord, orient = coordinates(checkedword, board, player)
     actualy_place(checkedword, board, vertcoord, horizontcoord, orient)
     npc.word_list.append(checkedword)
@@ -218,40 +237,43 @@ def place_word_npc(board, player):
 
 
 ##-----------TURN AND SCORE FUNCTIONS---------------
-def play_letter(maskboard, rowcol_moves, player, plays_list):
+#Function for a human player to play a letter
+def play_letter(mask, rowcol_moves, player, plays_list, board, hits):
   fullcoord = ''
   fullcheck = 0
-  hits = 0
+  booms = 0
   rights = 0
-  locations = []
   typehits = []
+  letterrow = []
   while fullcheck < 3:
     fullcoord = input(f"Choose Letter + Row/Column with 'in' (e.g AinA, AinD, Ain3, Ain12):\n")
     while len(fullcoord) < 3:
         print("Invalid play.")
         fullcoord = input(f"Choose Letter + Row/Column with 'in' (e.g AinA, AinD, Ain3, Ain12):\n")
+        if 'in' not in fullcoord:
+          print("Invalid play.")
+          fullcoord = input(f"Choose Letter + Row/Column with 'in' (e.g AinA, AinD, Ain3, Ain12):\n")
     letterrow = fullcoord.upper().split("IN")
-    print (letterrow)
+    while len(letterrow) < 2:
+          print("Invalid play.")
+          fullcoord = input(f"Choose Letter + Row/Column with 'in' (e.g AinA, AinD, Ain3, Ain12):\n")
+          letterrow = fullcoord.upper().split("IN")
     boardletter, rowcolumn = letterrow[0], letterrow[1]
-    print(boardletter, rowcolumn)
     if boardletter in letterdict:
       fullcheck += 1
-      print (f"letter check: {fullcheck}")
     else:
       fullcheck = 0
       print ("Invalid character. Please choose a letter from the English alphabet.")
     try:
       rowcolumn = int(rowcolumn)
-      if rowcolumn in numberdict:
+      if rowcolumn in numberdict and rowcolumn <= len(board):
         fullcheck += 1
-        print (f"coord check: {fullcheck}")
       else:
         fullcheck = 0
         print ("Invalid column.")
     except:
-      if rowcolumn in letterdict:
+      if rowcolumn in letterdict and letterdict.get(rowcolumn) <= len(board):
         fullcheck += 1
-        print (f"coord check: {fullcheck}")
       else:
         fullcheck = 0
         print ("Invalid row.")
@@ -262,83 +284,105 @@ def play_letter(maskboard, rowcol_moves, player, plays_list):
     else:
         fullcheck += 1
         plays_list.append(fullcoord.lower())
+  try:
+    rowcolumn = int(rowcolumn)
+  except:
+    rowcolumn = str(rowcolumn)
 
   if type(rowcolumn) == str:
     row = letterdict.get(rowcolumn)
     for i in range(len(board)):
+      if board[row][i] in wavez:
+        continue
       if board[row][i] == mask[row][i]:
         if mask[row][i].isalpha() == True:
           typehits.append(mask[row][i].lower())
         continue
-      elif board[row][i] in wavez:
-        continue
       else:
         mask[row][i] = '?'
-        hits += 1
+        booms += 1
         if board[row][i] == boardletter:
           mask[row][i] = boardletter
           typehits.append(boardletter.lower())
-          print (f"You hit '{boardletter}' in {rowcolumn}{i}!")
+          hit = boardletter+'in'+str(rowcolumn)+str(i)
+          hits.append(hit)
+          print (f"You hit '{boardletter}' in {rowcolumn}{i+1}!")
         else:
           typehits.append('.')
-          print (f"There's something in {rowcolumn}{i}.")
+          print (f"There's something in {rowcolumn}{i+1}.")
 
 
-  elif type(rowcolumn) == int:
+  if type(rowcolumn) == int:
     column = rowcolumn-1
     for i in range(len(board)):
+      if board[i][column] in wavez:
+        continue
       if board[i][column] == mask[i][column]:
         if mask[i][column].isalpha() == True:
           typehits.append(mask[i][column].lower())
         continue
-      elif board[i][column] in wavez:
-        continue
       else:
         mask[i][column] = '?'
-        hits += 1
+        booms += 1
         if board[i][column] == boardletter:
           mask[i][column] = boardletter
           typehits.append(boardletter.lower())
+          hit = boardletter+'in'+str(numberdict.get(i))+str(rowcolumn)
+          hits.append(hit)
           print (f"You hit '{boardletter}' in {numberdict.get(i)}{rowcolumn}!")
         else:
           typehits.append('.')
           print (f"There's something in {numberdict.get(i)}{rowcolumn}.")
 
   rowcol_regex = ''.join(typehits)
-  print (type(rowcolumn))
+  #print (type(rowcolumn))
+  if rowcol_regex == rowcol_moves.get(rowcolumn):
+    print('No new information.')
   if rowcol_moves.get(rowcolumn) != None:
     rowcol_moves.update({rowcolumn:rowcol_regex})
   else:
     rowcol_moves.setdefault(rowcolumn, rowcol_regex)
   return mask
 
-def score_calc(rowcol_regex, score):
+#Function to calculate the current score
+def score_calc(player, opponent):
   tempscore = 0
-  for i in list(rowcol_regex.keys()):
-    word = rowcol_regex.get(i)
-    tempscore += len(word)*5
-    for b in word:
-      if b.isalpha():
-        tempscore += 5
-    if word.lower() in beginner_word_list:
-      tempscore += 100
-  score.append(tempscore)
-  return score
+  tempscore += len(player.hits)*5
+  for i in list(player.rowcol_moves.keys()):
+    word = player.rowcol_moves.get(i)
+    for single in opponent.word_list:
+      if single.lower() in word.lower():
+        tempscore += 100
+  player.turn_score.append(tempscore)
+  player.score = tempscore
+  return player.score
 
-def get_percentage_bar(score, dict=diff_score, size=size):
+#Function to get decryption percentage and progress bar
+def get_percentage_bar(turn_score, player, size, dict=diff_score):
   bar = ''
-  if len(score) > 0:
-    newest = score[-1]
+  if len(turn_score) > 0:
+    newest = turn_score[-1]
     percentage = newest / diff_score.get(size)*100
+    player.turn_percentage.append(percentage)
     temp_percentage = round(percentage)
-    while temp_percentage > 9:
-      
+    while temp_percentage >= 10:
+
       temp_percentage -= 10
       bar += '▮'
     bar = bar.ljust(10, '▯')
-  if len(score) == 0:
+  if len(turn_score) == 0:
     percentage = 0
     bar = bar.ljust(10, '▯')
-  encrypt_line = f'Decryption progress: {round(percentage,2)}%\n[{bar}]\n'
-  return print(encrypt_line)
+  percent_line = f'Your decryption progress: {round(percentage,2)}%'
+  percent_bar = f'[{bar}]'
+  if player == npc:
+    percent_line = f'Enemy decryption progress: {round(percentage,2)}%'
+  return percent_line, percent_bar
+
+
+##Function for 1 complete player turn
+def player_turn(player, opponent, score, turn_score, rowcol_moves, mask, plays_list, board, hits, size):
+  play_letter(mask, rowcol_moves, player, plays_list, board, hits)
+  score_calc(player, opponent)
+  get_percentage_bar(turn_score, player, size)
 
