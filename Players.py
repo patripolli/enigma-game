cee068ddc02d56156d0d1cb373ddca9ae605b7a6
patripolli <143572467+patripolli@users.noreply.github.com>
2024-05-11 12:34:07 -0300

    Update Players.py
    Kept only the player classes in this file for organization

--- Players.py
+++ Players.py
@@ -1,29 +1,6 @@
-def set_difficulty():
-    difficulty = 0
-    while difficulty == 0:
-      difficulty = int(input(f'Please choose the difficulty:\n1 - Easy\n2 - Medium\n'))
-      if difficulty == 1:
-        print ('Difficulty set as Easy.')
-      elif difficulty == 2:
-        print ('Difficulty set as Medium.')
-      #IMPLEMENT DIFFICULTY   --   3 - Hard\n
-      #if difficulty == 3:
-        #print ('Difficulty set as Hard.')
-      else:
-        difficulty = 0
-        print ('Please choose a valid option.')
-    return difficulty
+#---------------PLAYER CLASSES---------------
 
-def make_word_list():
-    full_word_list = []
-    with open("/content/beginnerwords.txt", "r") as beginner_words:
-      for a in beginner_words:
-        b = a.replace('\n', '')
-        full_word_list.append(b)
-    return full_word_list
-
-
-
+#---------------PLAYER---------------
 class Player:
     """Basic player Class."""
     def __init__(self, name):
@@ -31,23 +8,27 @@ class Player:
       self.rowcol_moves = {}
       self.plays_list = []
       self.word_list = []
+      self.bkp_board = None
       self.board = None
       self.mask = None
+      self.hits = []
       self.score = 0
       self.turn_score = []
+      self.turn_percentage = []
 
     def set_board(self):
       board_len = 0
       while board_len == 0:
         match_board = new_board()
-        self.board, board_len = match_board[0], match_board[1]
+        self.bkp_board, board_len, size = match_board[0], match_board[1], match_board[2]
+        self.board = self.bkp_board.copy()
         #print (f"this is board{self.board}, of {board_len} length")
         if self.mask is None:
-          self.mask = p1.board.copy()
-      return self.board, self.mask, board_len
+          self.mask = self.board.copy()
+      return self.board, self.mask, board_len, size
 
     
-
+#---------------NPC---------------
 class NPC():
     """Basic NPC Class."""
     def __init__(self, difficulty):
